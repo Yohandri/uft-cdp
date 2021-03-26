bf8efa5adc941a00cd5cb59338fa1ea63d797717
Yohandri <yoha3001@gmail.com>
2021-03-26 10:33:07 -0400

    updata angular 11

--- src/app/components/payment-management/payment-management.component.spec.ts
+++ src/app/components/payment-management/payment-management.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaymentManagementComponent } from './payment-management.component';
 
@@ -6,7 +6,7 @@ describe('PaymentManagementComponent', () => {
   let component: PaymentManagementComponent;
   let fixture: ComponentFixture<PaymentManagementComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PaymentManagementComponent ]
     })
